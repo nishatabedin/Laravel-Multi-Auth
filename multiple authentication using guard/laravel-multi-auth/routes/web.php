f3de65508281d58d0f00272378e285ac33bf94bf
nishatabedin <mdnishatabedin@gmail.com>
2021-12-24 10:43:07 +0600

    laravel/ui package install

--- multiple authentication using guard/laravel-multi-auth/routes/web.php
+++ multiple authentication using guard/laravel-multi-auth/routes/web.php
@@ -16,12 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-
-Route::group(['middleware' => ['web']], function () {
-    Route::get('web-login', 'Auth\AuthController@webLogin');
-    Route::post('web-login', ['as' => 'web-login', 'uses' => 'Auth\AuthController@webLoginPost']);
-    Route::get('admin-login', 'AdminAuth\AuthController@adminLogin');
-    Route::post('admin-login', ['as' => 'admin-login', 'uses' => 'AdminAuth\AuthController@adminLoginPost']);
-});
